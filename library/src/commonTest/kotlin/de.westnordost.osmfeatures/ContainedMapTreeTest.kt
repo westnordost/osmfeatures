6e6fb3594c041bdd548bbbab8a3e06c9b785d057
Tobias Zwick <newton@westnordost.de>
2024-02-26 19:49:43 +0100

    remove some unnecessary type declarations

--- library/src/commonTest/kotlin/de.westnordost.osmfeatures/ContainedMapTreeTest.kt
+++ library/src/commonTest/kotlin/de.westnordost.osmfeatures/ContainedMapTreeTest.kt
@@ -7,20 +7,19 @@ import kotlin.test.assertTrue
 class ContainedMapTreeTest {
     @Test
     fun copes_with_empty_feature_collection() {
-        val t = tree(emptyList())
-        assertTrue(t.getAll(mapOf("a" to "b")).isEmpty())
+        assertTrue(tree().getAll(mapOf("a" to "b")).isEmpty())
     }
 
     @Test
     fun find_single_map() {
-        val f1: Map<String, String> = mapOf("a" to "b")
-        val t: ContainedMapTree<String, String> = tree(listOf(f1))
+        val f1 = mapOf("a" to "b")
+        val t = tree(f1)
         assertEquals(listOf(f1), t.getAll(mapOf("a" to "b", "c" to "d")))
     }
 
     @Test
-    fun dont_find_single_map() {
-        val tree: ContainedMapTree<String, String> = tree(listOf(mapOf("a" to "b")))
+    fun do_not_find_single_map() {
+        val tree = tree(mapOf("a" to "b"))
         assertTrue(tree.getAll(mapOf()).isEmpty())
         assertTrue(tree.getAll(mapOf("c" to "d")).isEmpty())
         assertTrue(tree.getAll(mapOf("a" to "c")).isEmpty())
@@ -28,25 +27,21 @@ class ContainedMapTreeTest {
 
     @Test
     fun find_only_generic_map() {
-        val f1: Map<String, String> = mapOf("a" to "b")
-        val f2: Map<String, String> = mapOf("a" to "b", "c" to "d")
-        val tree: ContainedMapTree<String, String> = tree(listOf(f1, f2))
+        val f1 = mapOf("a" to "b")
+        val f2 = mapOf("a" to "b", "c" to "d")
+        val tree = tree(f1, f2)
         assertEquals(listOf(f1), tree.getAll(mapOf("a" to "b")))
     }
 
     @Test
     fun find_map_with_one_match_and_with_several_matches() {
-        val f1: Map<String, String> = mapOf("a" to "b")
-        val f2: Map<String, String> = mapOf("a" to "b", "c" to "d")
-        val f3: Map<String, String> = mapOf("a" to "b", "c" to "e")
-        val f4: Map<String, String> = mapOf("a" to "b", "d" to "d")
-        val tree: ContainedMapTree<String, String> = tree(listOf(f1, f2, f3, f4))
+        val f1 = mapOf("a" to "b")
+        val f2 = mapOf("a" to "b", "c" to "d")
+        val f3 = mapOf("a" to "b", "c" to "e")
+        val f4 = mapOf("a" to "b", "d" to "d")
+        val tree = tree(f1, f2, f3, f4)
         assertEqualsIgnoreOrder(listOf(f1, f2), tree.getAll(mapOf("a" to "b", "c" to "d")))
     }
-
-    companion object {
-        private fun tree(items: Collection<Map<String, String>>): ContainedMapTree<String, String> {
-            return ContainedMapTree(items)
-        }
-    }
 }
+
+private fun tree(vararg items: Map<String, String>) = ContainedMapTree(items.toList())
--- library/src/commonTest/kotlin/de.westnordost.osmfeatures/StartsWithStringTreeTest.kt
+++ library/src/commonTest/kotlin/de.westnordost.osmfeatures/StartsWithStringTreeTest.kt
@@ -8,21 +8,20 @@ class StartsWithStringTreeTest {
 
     @Test
     fun copes_with_empty_collection() {
-        val t = StartsWithStringTree(listOf())
-        assertTrue(t.getAll("any").isEmpty())
+        assertTrue(tree().getAll("any").isEmpty())
     }
 
     @Test
     fun find_single_string() {
-        val t = StartsWithStringTree(listOf("anything"))
+        val t = tree("anything")
         assertEquals(listOf("anything"), t.getAll("a"))
         assertEquals(listOf("anything"), t.getAll("any"))
         assertEquals(listOf("anything"), t.getAll("anything"))
     }
 
     @Test
-    fun dont_find_single_string() {
-        val t = StartsWithStringTree(listOf("anything", "more", "etc"))
+    fun do_not_find_single_string() {
+        val t = tree("anything", "more", "etc")
         assertTrue(t.getAll("").isEmpty())
         assertTrue(t.getAll("nything").isEmpty())
         assertTrue(t.getAll("anything else").isEmpty())
@@ -30,7 +29,9 @@ class StartsWithStringTreeTest {
 
     @Test
     fun find_several_strings() {
-        val t = StartsWithStringTree(listOf("anything", "anybody", "anytime"))
+        val t = tree("anything", "anybody", "anytime")
         assertEqualsIgnoreOrder(listOf("anything", "anybody", "anytime"), t.getAll("any"))
     }
 }
+
+private fun tree(vararg strings: String) = StartsWithStringTree(strings.toList())
