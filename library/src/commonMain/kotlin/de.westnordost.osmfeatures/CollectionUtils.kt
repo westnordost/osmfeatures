23ba6518b011fc07ded7c7b2eb4a51fa9551a445
Ercalvez <erwancalv@gmail.com>
2024-03-09 00:51:24 +0100

    synchronized map using `lazy` operator

--- library/src/commonMain/kotlin/de.westnordost.osmfeatures/CollectionUtils.kt
+++ library/src/commonMain/kotlin/de.westnordost.osmfeatures/CollectionUtils.kt
@@ -3,14 +3,15 @@ package de.westnordost.osmfeatures
 /** For the given map, get the value of the entry at the given key and if there is no
  * entry yet, create it using the given create function thread-safely  */
 internal fun <K, V> MutableMap<K, V>.synchronizedGetOrCreate(key: K, createFn: (K) -> V): V {
-    synchronized(this) {
-        val value = get(key)
-        return if (value == null) {
-            val answer = createFn(key)
-            put(key, answer)
-            answer
-        } else {
-            value
+        val value by lazy {
+            val value = get(key)
+            if (value == null) {
+                val answer = createFn(key)
+                put(key, answer)
+                answer
+            } else {
+                value
+            }
         }
-    }
+        return value
 }
