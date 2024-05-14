b6f3e0c9015b33de26fc5ee4f8f5a6712ef459b5
Tobias Zwick <newton@westnordost.de>
2024-05-14 02:19:40 +0200

    add link to ticket to monitor

--- src/commonMain/kotlin/de/westnordost/osmfeatures/JsonUtils.kt
+++ src/commonMain/kotlin/de/westnordost/osmfeatures/JsonUtils.kt
@@ -5,6 +5,7 @@ import kotlinx.io.readString
 import kotlinx.serialization.json.*
 
 // TODO This can hopefully be replaced with a function from kotlinx-serialization soon that streams the data
+// monitor https://github.com/Kotlin/kotlinx.serialization/issues/253
 @OptIn(ExperimentalStdlibApi::class)
 internal inline fun <reified T> Json.decodeFromSource(source: Source): T =
     decodeFromString(source.use { it.readString() })
