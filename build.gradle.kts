fe1a420014bfdd9520a66ad96738de50241a2920
Tobias Zwick <newton@westnordost.de>
2025-11-13 18:27:52 +0100

    update plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform") version "2.1.0"
-    id("com.android.library") version "8.12.0"
-    id("com.vanniktech.maven.publish") version "0.34.0"
+    id("com.android.library") version "8.12.3"
+    id("com.vanniktech.maven.publish") version "0.35.0"
     id("org.jetbrains.dokka") version "2.1.0"
 }
 
